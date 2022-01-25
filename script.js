99336a216b33c9d4d43c0f55d375199c76fb751a
sanmoyano <smoyano.di@gmail.com>
2022-01-25 11:05:57 -0300

    cargarDom

--- script.js
+++ script.js
@@ -14,10 +14,26 @@ let divGastos = document.querySelector(".li-gastos-cotainer");
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
-    let datForm = new FormData(e.target);
+
+    let datForm = new FormData(e.target);//e.target accedo al formulario
     let gasto = new Gasto (datForm.get('monto'), datForm.get('nombre'));
     gastos.push(gasto);
 
     localStorage.setItem('gastos', JSON.stringify(gastos));
     formulario.reset();
-})
+
+    cargarDom();
+
+});
+
+
+function cargarDom () {
+    let gastosParseados = JSON.parse(localStorage.getItem('monots'));
+
+    gastosParseados.forEach((gasto, indice) => {
+        divGastos.innerHTML += `
+            
+        `
+    });
+}
+
