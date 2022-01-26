8e665642b8a5d26292205e6521676b07d49f8caf
sanmoyano <smoyano.di@gmail.com>
2022-01-26 09:40:12 -0300

    gastos form

--- script.js
+++ script.js
@@ -10,30 +10,67 @@ class Gasto {
 
 let gastos = [];
 let formulario = document.querySelector("#form-gastos");
-let divGastos = document.querySelector(".li-gastos-cotainer");
+let divGastos = document.querySelector(".li-gastos-container");
+let empty = document.querySelector(".empty");
+let emptyBtn = document.querySelector(".btn-empty");
+let totalGastos = document.querySelector(".total-gastos")
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
 
     let datForm = new FormData(e.target);//e.target accedo al formulario
-    let gasto = new Gasto (datForm.get('monto'), datForm.get('nombre'));
-    gastos.push(gasto);
+    let gasto = new Gasto (datForm.get('monto'), datForm.get('nombre'));//creo el objeto con las propiedades de la clase
+    gastos.push(gasto);//mando al array
 
-    localStorage.setItem('gastos', JSON.stringify(gastos));
+    localStorage.setItem('gastos', JSON.stringify(gastos));//array al LS
     formulario.reset();
 
-    cargarDom();
+    cargarGastosDom();
+    mostrarTotal ();
 
 });
 
+//EVENTOS
+//eliminar historial
+emptyBtn.addEventListener('click', () => {
+    localStorage.clear('gastos');
+    divGastos.innerHTML = "";
+    totalGastos.innerHTML = "";
+    empty.style.display = "block";
+})
 
-function cargarDom () {
-    let gastosParseados = JSON.parse(localStorage.getItem('monots'));
-
+//FUNCIONES
+//cargar gastos al dom
+function cargarGastosDom () {
+    let gastosParseados = JSON.parse(localStorage.getItem('gastos'));
+    divGastos.innerHTML = "";
+    if (gastosParseados !== "") {
+        gastosParseados.forEach((gasto, indice) => {
+            divGastos.innerHTML += `
+                <li id="gastos ${indice + 1}" class="li-gastos-row"> 
+                        <p> $${gasto.monto} </p>
+                        <p> ${gasto.nombre} </p>
+                        <button class="btn-delete" id="boton ${indice + 1}"> X </button>
+                    </div>
+                </li>
+            `
+        });
+        empty.style.display = "none";
+        emptyBtn.style.display = "flex";
+    };
+    
+    //eliminar gastos individuales
     gastosParseados.forEach((gasto, indice) => {
-        divGastos.innerHTML += `
-            
-        `
-    });
+        document.getElementById(`boton ${indice + 1}`).addEventListener('click', () => {
+            divGastos.removeChild(document.getElementById(`gastos ${indice +1}`))
+        })
+    })
 }
 
+//mostrar total en DOM 
+function mostrarTotal () {
+    totalGastos.innerHTML = "";
+    totalGastos.innerHTML += `
+        <h3 class="total"> Total: $$$ </h3> 
+    `
+}
